3b1cc44766c42281b95badf0a43f1bf77c5bb003
Suraj <suraj@masterblocks.co.in>
2024-04-23 02:22:44 +0530

    All new changes commited

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/login';
+import Signup from './components/Signup';
+import TicketList from './components/TicketList';
+import CreateTicket from './components/CreateTicket';
+import Dashboard from './components/Dashboard';
+import { updateTicket } from './apiServer';
+import UpdateTicket from './components/UpdateTicket';
+import UpdateOne from './components/updateOne';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Routes>
+        <Route  path="/" Component={Login} />
+        <Route  path="/signup" Component={Signup} />
+        <Route path="/tickets" Component={TicketList} />
+        <Route path="/createTicket" Component={CreateTicket} />
+        <Route path="/dashboard" Component={Dashboard} />
+        <Route path="/tickets/${ticketId" Component={UpdateTicket} />
+        <Route path="/tickets/update/:id" Component={UpdateOne} />
+        </Routes>   
+    </Router>
   );
-}
+};
 
 export default App;
